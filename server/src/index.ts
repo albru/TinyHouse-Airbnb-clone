f5ef98641898ffbb29dcef8ce9e964eb06ac91c0
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2020-06-07 22:54:03 +0200

    add mock data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,26 @@
+import { listings } from "./listings";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+const port = 7000;
 
-const port = 9999;
+app.use(bodyParser.json());
 
 app.get("/", (_req, res) => res.send("<h1>My App</h1>"));
+
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+  listings.forEach((item, i) => {
+    if (item.id === id) {
+      res.send(listings.splice(i, 1));
+    }
+  });
+  return res.send("failed to delete listing");
+});
 
 app.listen(port, () => console.log("server started!;)"));
